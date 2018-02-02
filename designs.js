31d77dfa47ac36184de34896f44d0c965040356d
Matt <matthewcranford314@gmail.com>
2018-02-01 16:35:03 -0800

    Add mouseover color

--- designs.js
+++ designs.js
@@ -20,17 +20,35 @@ $(document).ready(function() {
         makeGrid(height,width);
     })
     function makeGrid(row,data) {
-    // Your code goes here!
-    console.log(typeof row);
-    console.log(row);
-    for(var i = 0; i < row; i++) {
-        $("#pixelCanvas").append("<tr></tr>");
-        console.log(i);
-        for(var j = 0; j < data; j++) {
-            $("tr").last().append("<td></td>"); 
+        // Your code goes here!
+        console.log(typeof row);
+        console.log(row);
+        for(var i = 0; i < row; i++) {
+            $("#pixelCanvas").append("<tr></tr>");
+            console.log(i);
+            for(var j = 0; j < data; j++) {
+                $("tr").last().append("<td></td>"); 
+            }
         }
+                
     }
+    console.log(isMouseDown);
+    var isMouseDown = false;
 
-    }
-    
+    $("body").mousedown(function() {
+        isMouseDown = true;
+        console.log(isMouseDown)
+    })
+    .mouseup(function() {
+        isMouseDown = false;
+        console.log(isMouseDown);
+    });
+
+    $("#pixelCanvas").on("mouseenter","td",function() {
+        console.log("!");
+        if(isMouseDown) {
+            $(this).css("background-color",color);
+        }
+    });      
+   
 });
