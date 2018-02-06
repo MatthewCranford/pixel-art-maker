7040dcce54bbc406fbe47a8dcecf1b2097043e01
Matt <matthewcranford314@gmail.com>
2018-02-05 18:22:32 -0800

    Work on grid

--- designs.js
+++ designs.js
@@ -1,92 +1,94 @@
 $(document).ready(function() {
 
-    // Select color input
-    var color = $("#colorPicker").val()
-    console.log(color);
+	// Select color input
+	var color = $("#colorPicker").val()
+	console.log(color);
 
-    $("#colorPicker").change(function() {
-        color = $("#colorPicker").val();
-        console.log(color);
-    })
-    
-    // Select size input
-   
-    // When size is submitted by the user, call makeGrid()
-    $("#create-btn").click(function(event) {
-        var height = $("#inputHeight").val();
-        var width = $("#inputWidth").val();
-        event.preventDefault();
-        if ($("#pixelCanvas tr").length < 1) {
-            makeGrid(height,width);
-        }
-        else if ($("#pixelCanvas tr").length < height) {
-            console.log("working!");
-            addHeight(height,width);
-        }
-        else if ($("#pixelCanvas tr td").length < width) {
-            addWidth(height,width);
-        }
-        
-    });
+	$("#colorPicker").change(function() {
+		color = $("#colorPicker").val();
+		console.log(color);
+	})
 
-    $("#clear-btn").click(function(event) {
-        $("#pixelCanvas").empty();
-        event.preventDefault();
-    });
+	// Select size input
 
-    function makeGrid(height,width) {
-        // Your code goes here!
+	// When size is submitted by the user, call makeGrid()
+	$("#create-btn").click(function(event) {
+		var height = $("#inputHeight").val();
+		var width = $("#inputWidth").val();
+		event.preventDefault();
+		if ($("#pixelCanvas tr").length < 1) {
+			makeGrid(height,width);
+		}
+		// else if ($("#pixelCanvas tr").length < height) {
+		// 	console.log("Calling addHeight");
+		// 	addHeight(height,width);
+		// }
+		// else if ($("#pixelCanvas tr td").length < width) {
+		// 	console.log("Calling addWidth");
+		// 	addWidth(height,width);
+		// }
 
-        for(var i = 0; i < height; i++) {
-            $("#pixelCanvas").append("<tr></tr>");
-            for(var j = 0; j < width; j++) {
-                $("tr").last().append("<td></td>"); 
-            }
-        }            
-    }
+	});
 
-    function addHeight(height,width) {
-        for(var i = $("#pixelCanvas tr").length; i < height; i++) {
-            $("#pixelCanvas").append("<tr></tr>");
-            for(var j = 0; j < width; j++) {
-                $("tr").last().append("<td></td>"); 
-            }
-        }
-    }
+	$("#clear-btn").click(function(event) {
+		$("#pixelCanvas").empty();
+		event.preventDefault();
+	});
 
-    function addWidth(height,width) {
-        for($("#pixelCanvas tr").each(function () {
-            
-        })) {
-            console.log("working");
-            for(var j = $("#pixelCanvas tr td").length; j < width; j++) {
-                $("tr").last().append("<td></td>"); 
-            }
-        }
-    }
+	function makeGrid(height,width) {
+		// Your code goes here!
+
+		for(var i = 0; i < height; i++) {
+			$("#pixelCanvas").append("<tr></tr>");
+			for(var j = 0; j < width; j++) {
+				$("tr").last().append("<td></td>");
+			}
+		}
+	}
+
+	// function addHeight(height,width) {
+	// 	for(var i = $("#pixelCanvas tr").length; i < height; i++) {
+	// 	  $("#pixelCanvas").append("<tr></tr>");
+	// 	  for(var j = 0; j < width; j++) {
+	// 			$("tr").last().append("<td></td>");
+	// 	  }
+	// 	}
+	// }
+
+	// function addWidth(height,width) {
+	// 	// for(var i = $("#pixelCanvas tr").length; i < height; i++) {
+	// 	// 	for(var j = $("#pixelCanvas tr td").length; j < width; j++) {
+			
+	// 			$("tr").each(function() {
+	// 				$(this).append("<td></td>");
+	// 			})
+	// 		}
+	// 	}
+	// }
 
 
-    var isMouseDown = false;
 
-    $("body").mousedown(function() {
-        isMouseDown = true;
-        console.log(isMouseDown)
-    })
-    .mouseup(function() {
-        isMouseDown = false;
-        console.log(isMouseDown);
-    });
-    
-    $("#pixelCanvas").on("mousedown","td", function() {
-        $(this).css("background-color",color);
-    });
+	var isMouseDown = false;
 
-    $("#pixelCanvas").on("mouseenter","td",function(event) {
-        console.log("!");
-        event.preventDefault();
-        if(isMouseDown) {
-            $(this).css("background-color",color);
-        }
-    });      
-   
+	$("body").mousedown(function() {
+		isMouseDown = true;
+		console.log(isMouseDown)
+	})
+	.mouseup(function() {
+		isMouseDown = false;
+		console.log(isMouseDown);
+	});
+
+	$("#pixelCanvas").on("mousedown","td", function() {
+		$(this).css("background-color",color);
+	});
+
+	$("#pixelCanvas").on("mouseenter","td",function(event) {
+		console.log("!");
+		event.preventDefault();
+		if(isMouseDown) {
+			$(this).css("background-color",color);
+		}
+	});
+
 });
